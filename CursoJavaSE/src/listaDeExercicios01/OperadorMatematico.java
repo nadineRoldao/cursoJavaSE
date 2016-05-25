57a7045d306173de2f0caf3bcc45d0ea554673c5
nadineRoldao <vitorianadineroldao@gmail.com>
2016-05-24 23:14:58 -0300

    adicionado exercicio que mostra como e o operado matematico

---
+++ CursoJavaSE/src/listaDeExercicios01/OperadorMatematico.java
@@ -0,0 +1,12 @@
+package listaDeExercicios01;
+
+public class OperadorMatematico {
+
+	public static void main(String[] args) {
+		double x = 7 + 4;
+		String y = "Oi" + " Nadine";
+		System.out.println(x);
+		System.out.println(y);
+	}
+	
+}
