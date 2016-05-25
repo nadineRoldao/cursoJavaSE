cbc88d040edb88862ee008dcfebac591a5d3afb0
nadineRoldao <vitorianadineroldao@gmail.com>
2016-05-24 23:11:04 -0300

    adicionado exercicio que mostra como sao os operadores de comparacao

---
+++ CursoJavaSE/src/listaDeExercicios01/OperadoresDeComparacao.java
@@ -0,0 +1,22 @@
+package listaDeExercicios01;
+
+public class OperadoresDeComparacao {
+
+public static void main(String[] args) {
+		
+		Integer numero01 = 10;
+		Integer numero02 = 20;
+		String resultado = "";
+
+		if(numero02 == numero02){
+			resultado = "Os numeros sao iguais";
+		}else {
+			resultado = "Os numeros sao diferentes";
+		}
+
+		System.out.println();
+		System.out.println(resultado);
+		System.out.println();
+	}
+	
+}
