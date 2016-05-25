05af253f70c69714e739455b2d464d450cd4fc9b
nadineRoldao <vitorianadineroldao@gmail.com>
2016-05-24 23:04:23 -0300

    adicionado exercicio que mostra a declaracao de variavel na mesma linha

---
+++ CursoJavaSE/src/listaDeExercicios01/DeclaracaoVariavelMesmaLinha.java
@@ -0,0 +1,29 @@
+package listaDeExercicios01;
+
+public class DeclaracaoVariavelMesmaLinha {
+
+public static void main(String[] args) {
+		
+		// Declarando mais de uma variável na mesma linha
+		int a, b, c, d;
+
+		// Declarando e atribuindo valor para mais de uma variável na mesma
+		String firstName = "Nadine", lastName = "Roldao";
+
+		a = 1;
+		b = 2;
+		c = 3;
+		d = 4;
+
+		System.out.println();
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println();
+
+	}
+	
+}
