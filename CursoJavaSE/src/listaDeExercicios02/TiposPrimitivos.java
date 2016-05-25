cbb350c0c554d884dda93b1da1fc3207c9efea99
nadineRoldao <vitorianadineroldao@gmail.com>
2016-05-24 23:36:03 -0300

    adicionado exercicio que mostra todos os tipos primitivos

---
+++ CursoJavaSE/src/listaDeExercicios02/TiposPrimitivos.java
@@ -0,0 +1,32 @@
+package listaDeExercicios02;
+
+/*
+nome:tipos primitivos
+data:19/04
+número:exercício 03
+*/
+public class TiposPrimitivos {
+
+public static void main(String[] args) {
+	
+	
+	int idade = 13;
+	double peso = 43.8;
+	char sexo = 'M';
+	boolean casado = false;
+	short quantasLetrasTemMeuNome = 6;
+	long quantasLetrasTemMeuNomeCompleto = 32l;
+	float quantasSilabasTemMeuNomeCompleto = 16f;
+
+	System.out.println("variavel do tipo int: " + idade);
+	System.out.println("variavel do tipo double: " + peso);
+	System.out.println("variavel do tipo booblean: " + sexo);
+	System.out.println("variavel do tipo char: " + casado);
+	System.out.println("variavel do tipo short: " + quantasLetrasTemMeuNome);
+	System.out.println("variavel do tipo long: " + quantasLetrasTemMeuNomeCompleto);
+	System.out.println("variavel do tipo float: " + quantasSilabasTemMeuNomeCompleto);
+
+
+}
+
+}
